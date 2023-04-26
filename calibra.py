b260272d9a5d98b2fcfbae34806ee3cccb2244aa
Anderson <anderfrasao52@gmail.com>
2023-04-26 17:30:49 -0300

    quase bom

--- calibra.py
+++ calibra.py
@@ -1,23 +1,26 @@
 import numpy as np
+from matplotlib import pyplot as pl
 import cv2 as cv
 import glob
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+objp = np.zeros((5*5,3), np.float32)
+objp[:,:2] = np.mgrid[0:5,0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('./imagens/*.jpg')
+images = glob.glob('./imagens2/*.jpg')
 
 for fname in images:
     print(fname)
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
+    # pl.figure(figsize=(10, 6))
+    # pl.imshow(gray, 'gray')
+    # pl.show()
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,7), None)
+    ret, corners = cv.findChessboardCorners(gray, (5,5), None)
     
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -26,15 +29,16 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,7), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.drawChessboardCorners(img, (5,5), corners2, ret)
+        pl.figure(figsize=(10, 6))
+        pl.imshow(img)
+        pl.show()
 
 cv.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-img = cv.imread('foto7.jpg')
+img = cv.imread('./imagens2/foto7.jpg')
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
