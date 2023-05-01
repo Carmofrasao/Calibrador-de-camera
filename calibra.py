2c163ea1efbf90d866f8d239dba52527380ff53c
Anderson <anderfrasao52@gmail.com>
2023-04-30 22:55:10 -0300

    usando as 2 calibragens

--- calibra.py
+++ calibra.py
@@ -52,9 +52,9 @@ dst = dst[y:y+h, x:x+w]
 cv.imwrite('calibresult.png', dst)
 
 # undistort
-# mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-# dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-# # crop the image
-# x, y, w, h = roi
-# dst = dst[y:y+h, x:x+w]
-# cv.imwrite('calibresult.png', dst)
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult2.png', dst)
